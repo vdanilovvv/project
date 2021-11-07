079f37d05d511c497643c3f6ccd1385e8fd424a2
vdanilov <seva-danilov@mail.ru>
2021-11-08 00:10:53 +0300

    PleaseHelp

--- src/test/java/Browser.java
+++ src/test/java/Browser.java
@@ -1,17 +1,25 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.core.config.Order;
+import org.junit.*;
 
 import org.apache.logging.log4j.LogManager;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Browser {
+
+
 
     private org.apache.logging.log4j.Logger logger = LogManager.getLogger(Browser.class);
     protected WebDriver driver;
@@ -20,7 +28,7 @@ public class Browser {
     public void StartUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
         driver.manage().window().maximize();
         logger.info("Драйвер поднят");
     }
@@ -31,37 +39,156 @@ public class Browser {
             driver.quit();
     }
 
-    @Test
-    public void task1() {
-        driver.quit();
-        ChromeOptions option = new ChromeOptions();
-        option.addArguments("headless");
-        driver = new ChromeDriver(option);
-        driver.get("https://duckduckgo.com/");
-        driver.findElement(By.xpath("//*[@id=\"search_form_input_homepage\"]")).sendKeys("ОТУС");
-        driver.findElement(By.xpath("//*[@id=\"search_button_homepage\"]")).click();
-        Assert.assertEquals("Онлайн‑курсы для профессионалов, дистанционное обучение", driver.findElement(By.xpath("//*[@id=\"r1-0\"]/div/h2/a[1]")).getText());
-    }
 
-    @Test
-    public void task2() {
-        driver.quit();
-        ChromeOptions option = new ChromeOptions();
-        option.addArguments("--kiosk");
-        driver = new ChromeDriver(option);
-        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
-        driver.get("https://demo.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_ga=2.181802926.889871791.1632394818-2083132868.1632394818");
-        driver.findElement(By.xpath("/html/body/section[2]/div/ul[2]/li[1]/span/a/div[1]")).click();
-        Assert.assertEquals("pp_hoverContainer", driver.findElement(By.xpath("/html/body/div[8]/div[3]/div/div/div/div[2]/div[1]")).getAttribute("class"));
-    }
+    final String RUS_FIRST_NAME = "Всеволод";
+    final String ENG_FIRST_NAME = "Vsevolod";
+    final String RUS_SECOND_NAME = "Данилов";
+    final String ENG_SECOND_NAME = "Danilov";
+    final String BLOG_NAME = "DanilovOTUS";
+    final String BIRTHDAY = "29.09.1994";
+    final String VK_CONTACT = "vk.com/danilov_test";
+    final String TG_CONTACT = "@danilovTest";
+    final String COMPANY = "Тестовая компания";
+    final String POSITION = "Тестировщик";
+    String login = System.getProperty("login");
+    String password = System.getProperty("password");
 
-    @Test
-    public void task3() {
+
+
+    public void aboutMe() {
         driver.get("https://otus.ru");
-        driver.findElement(By.cssSelector("button.header2__auth")).click();
-        driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[3]/div[2]/div[2]/form/div[2]/input")).sendKeys("gacojib958@otozuz.com");
-        driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[3]/div[2]/div[2]/form/div[3]/input")).sendKeys("qwer1234");
-        driver.findElement(By.cssSelector("button.new-button")).click();
-        logger.info(driver.manage().getCookies());
+        driver.findElement(By.xpath("//*[contains(text(),'и регистрация')]/parent::button")).click();
+        driver.findElement(By.xpath("//form[@action='/login/']/descendant::input[@placeholder='Электронная почта']")).sendKeys(login);
+        driver.findElement(By.xpath("//input[@placeholder='Введите пароль']")).sendKeys(password);
+        driver.findElement(By.xpath("//form[@action='/login/']//*[contains(text(),'Войти')]")).click();
+        driver.findElement(By.xpath("//p[contains(@class,'username')]")).click();
+        driver.findElement(By.xpath("//div[@class='header2-menu__dropdown-text']")).click();
     }
+
+
+    @Test
+    public void test1InputData() throws InterruptedException {
+       aboutMe();
+       driver.findElement(By.xpath("//*[@id=\"id_fname\"]")).clear();
+       driver.findElement(By.xpath("//*[@id=\"id_fname\"]")).sendKeys(RUS_FIRST_NAME);
+       driver.findElement(By.xpath("//*[@id=\"id_fname_latin\"]")).clear();
+       driver.findElement(By.xpath("//*[@id=\"id_fname_latin\"]")).sendKeys(ENG_FIRST_NAME);
+       driver.findElement(By.xpath("//*[@id=\"id_lname\"]")).clear();
+       driver.findElement(By.xpath("//*[@id=\"id_lname\"]")).sendKeys(RUS_SECOND_NAME);
+       driver.findElement(By.xpath("//*[@id=\"id_lname_latin\"]")).clear();
+       driver.findElement(By.xpath("//*[@id=\"id_lname_latin\"]")).sendKeys(ENG_SECOND_NAME);
+       driver.findElement(By.xpath("//*[@id=\"id_blog_name\"]")).clear();
+       driver.findElement(By.xpath("//*[@id=\"id_blog_name\"]")).sendKeys(BLOG_NAME);
+       driver.findElement(By.xpath("//*[@title='День рождения']")).clear();
+       driver.findElement(By.xpath("//*[@title='День рождения']")).sendKeys(BIRTHDAY);
+       driver.findElement(By.xpath("//*[@name='country']/following-sibling::div")).click();
+       driver.findElement(By.xpath("//*[@title='Россия']")).click();
+       Thread.sleep(1500);
+       driver.findElement(By.xpath("//*[@name='city']/following-sibling::div")).click();
+       driver.findElement(By.xpath("//*[@title='Москва']")).click();
+       driver.findElement(By.xpath("//*[@name='english_level']/following-sibling::div")).click();
+       driver.findElement(By.xpath("//*[@title='Начальный уровень (Beginner)']")).click();
+
+       if ((!driver.findElement(By.xpath("//*[@id='id_ready_to_relocate_1']")).isSelected())){
+           driver.findElement(By.xpath("//*[@id='id_ready_to_relocate_1']/..")).click();
+       }
+        if ((!driver.findElement(By.xpath("//*[@title='Полный день']")).isSelected())){
+            driver.findElement(By.xpath("//*[@title='Полный день']/..")).click();
+        }
+
+       if (driver.findElements(By.xpath("//input[@value='" + VK_CONTACT + "']")).isEmpty()){
+           driver.findElement(By.xpath("//button[text()='Добавить']")).click();
+           driver.findElement(By.xpath("//span[text()='Способ связи']/..")).click();
+           driver.findElement(By.xpath("//span[text()='Способ связи']/../../following-sibling::div/descendant::button[@title='VK']")).click();
+           driver.findElement(By.xpath("//div[text()='VK']/../../following-sibling::input")).clear();
+           driver.findElement(By.xpath("//div[text()='VK']/../../following-sibling::input")).sendKeys(VK_CONTACT);
+       }
+
+
+        if (driver.findElements(By.xpath("//input[@value='" + TG_CONTACT + "']")).isEmpty()){
+            driver.findElement(By.xpath("//button[text()='Добавить']")).click();
+            driver.findElement(By.xpath("//span[text()='Способ связи']/..")).click();
+            driver.findElement(By.xpath("//span[text()='Способ связи']/../../following-sibling::div/descendant::button[@title='Тelegram']")).click();
+            driver.findElement(By.xpath("//div[text()='Тelegram']/../../following-sibling::input")).clear();
+            driver.findElement(By.xpath("//div[text()='Тelegram']/../../following-sibling::input")).sendKeys(TG_CONTACT);
+        }
+        if ((!driver.findElement(By.xpath("//*[@id='id_is_email_preferable']")).isSelected())){
+            driver.findElement(By.xpath("//*[@id='id_is_email_preferable']/parent::*")).click();
+        }
+
+        driver.findElement(By.xpath("//*[@id=\"id_gender\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"id_gender\"]/option[2]")).click();
+        driver.findElement(By.xpath("//*[@id=\"id_company\"]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"id_company\"]")).sendKeys(COMPANY);
+        driver.findElement(By.xpath("//*[@id=\"id_work\"]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"id_work\"]")).sendKeys(POSITION);
+
+
+        if ((driver.findElements(By.xpath("//*[@class='experience-row__remove ic-close js-formset-delete']")).isEmpty())){
+            driver.findElement(By.xpath("//a[@class='experience-add js-formset-add']")).click();
+            driver.findElement(By.xpath("//*[@id=\"id_experience-0-experience\"]")).click();
+            driver.findElement(By.xpath("//*[@id=\"id_experience-0-experience\"]/option[3]")).click();
+        }
+        driver.findElement(By.xpath("//*[@title='Сохранить и продолжить']")).click();
+
+    }
+
+
+
+
+    @Test
+    public void test2CheckData() throws InterruptedException {
+        aboutMe();
+        Assert.assertEquals(RUS_FIRST_NAME, driver.findElement(By.xpath("//*[@id=\"id_fname\"]")).getAttribute("value"));
+        Assert.assertEquals(ENG_FIRST_NAME, driver.findElement(By.xpath("//*[@id=\"id_fname_latin\"]")).getAttribute("value"));
+        Assert.assertEquals(RUS_SECOND_NAME, driver.findElement(By.xpath("//*[@id=\"id_lname\"]")).getAttribute("value"));
+        Assert.assertEquals(ENG_SECOND_NAME, driver.findElement(By.xpath("//*[@id=\"id_lname_latin\"]")).getAttribute("value"));
+        Assert.assertEquals(BLOG_NAME, driver.findElement(By.xpath("//*[@id=\"id_blog_name\"]")).getAttribute("value"));
+        Assert.assertEquals(BIRTHDAY, driver.findElement(By.xpath("//*[@title='День рождения']")).getAttribute("value"));
+        Assert.assertEquals("Россия", driver.findElement(By.xpath("//*[@name='country']/following-sibling::div")).getText());
+        Assert.assertEquals("Москва", driver.findElement(By.xpath("//*[@data-title='Город']/following-sibling::div")).getText());
+        Assert.assertEquals("Начальный уровень (Beginner)", driver.findElement(By.xpath("//*[@data-title='Уровень знания английского языка']/following-sibling::div")).getText());
+        Assert.assertEquals(true, driver.findElement(By.cssSelector("#id_ready_to_relocate_1")).isSelected());
+        Assert.assertEquals(true, driver.findElement(By.xpath("//*[@id='id_ready_to_relocate_1']")).isSelected());
+        Assert.assertEquals("Тelegram", driver.findElement(By.xpath("//*[@value='telegram']/following-sibling::div")).getText());
+        Assert.assertEquals(TG_CONTACT, driver.findElement(By.cssSelector("#id_contact-0-value")).getAttribute("value"));
+        Assert.assertEquals("VK", driver.findElement(By.xpath("//*[@value='vk']/following-sibling::div")).getText());
+        Assert.assertEquals(VK_CONTACT, driver.findElement(By.cssSelector("#id_contact-1-value")).getAttribute("value"));
+        Assert.assertEquals(true, driver.findElement(By.cssSelector("#id_is_email_preferable")).isSelected());
+        Assert.assertEquals("Мужской", driver.findElement(By.xpath("//*[@id='id_gender']/descendant::option[@selected='']")).getText());
+        Assert.assertEquals("Тестовая компания", driver.findElement(By.xpath("//*[@id=\"id_company\"]")).getAttribute("value"));
+        Assert.assertEquals("Тестировщик", driver.findElement(By.xpath("//*[@id=\"id_work\"]")).getAttribute("value"));
+        Assert.assertEquals("Java", driver.findElement(By.xpath("//*[@id='id_experience-0-experience']/descendant::option[@selected='']")).getText());
+        Assert.assertEquals("Только начал", driver.findElement(By.xpath("//*[@id='id_experience-0-level']/descendant::option[@selected='']")).getText());
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //        Assert.assertEquals("true", driver.findElement(By.xpath("/html/body/div[1]/div/div[5]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[1]/div/div[1]/div[4]/div[2]/label[2]/span")).getAttribute("checked"));
+
+
+
+
+
+
+
+
+
+
+        Thread.sleep(2000);
+
+    }
+
+
+
+
 }
